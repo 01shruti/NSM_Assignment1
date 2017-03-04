a7818b1cbcd40bf6ef18256a7489e6e454171dbb
01shruti <shruti.goyal94@gmail.com>
2017-03-04 18:12:59 +0000

    D_v3

--- dijkstra.py
+++ dijkstra.py
@@ -1,11 +1,60 @@
 
-from heapq import heappop, heappush
+from heapq import heappop, heappush, heapify
 import networkx as nx
 from itertools import count
 import matplotlib.pyplot as plt
 
-#### Directed Dijkstra Algorithm ####
 
+class P_Dict(dict):
+    def __init__(self, *args, **kwargs):
+        super(P_Dict, self).__init__(*args, **kwargs)
+        self._rebuild_heap()
+
+    def _rebuild_heap(self):
+        self._heap = [(v, k) for k, v in self.items()]
+        heapify(self._heap)
+
+    def pop_smallest(self):
+        heap = self._heap
+        v, k = heappop(heap)
+        while k not in self or self[k] != v:
+            v, k = heappop(heap)
+        del self[k]
+        return k
+
+    def __setitem__(self, key, val):
+        super(P_Dict, self).__setitem__(key, val)
+
+        if len(self._heap) < 2 * len(self):
+            heappush(self._heap, (val, key))
+        else:
+            self._rebuild_heap()
+
+def dijkstra(G,v):
+    dist_Q = P_Dict()
+    dist_Q[v] = 0
+    final_dist = {}
+    dist = {}
+    dist[v] = 0
+    pred = {}
+    while len(final_dist) <len(G) and len(dist_Q) != 0:
+        w = dist_Q.pop_smallest()
+        final_dist[w] = dist[w]
+
+        for x in G[w]:
+            if x not in final_dist:
+                if x not in dist:
+                    dist[x] = final_dist[w] + G[w][x]['weight']
+                    dist_Q[x] = final_dist[w] + G[w][x]['weight']
+                elif final_dist[w] + G[w][x]['weight'] < dist[x]:
+                    dist[x] = final_dist[w] + G[w][x]['weight']
+                    dist_Q[x] = final_dist[w] + G[w][x]['weight']
+                    pred[x] = w
+    return final_dist
+
+
+#### Directed Dijkstra Algorithm ####
+'''
 def dijkstra(graph, source, destination, weight = 'weight'):
     if source == destination:
         return({source: 0}, {source: [source]})
@@ -53,22 +102,25 @@ def shortest_path(graph, source, target, weight = 'weight'):
         raise nx.NetworkXNoPath(
             "node %s not reachable from %s" % (source, target))
 
-
+'''
 G = nx.DiGraph()
-G.add_edge('a','b',weight=1)
-G.add_edge('a','f',weight=0.5)
-G.add_edge('b','c',weight=0.5)
-G.add_edge('c','d',weight=0.5)
-G.add_edge('f','e',weight=0.6)
-G.add_edge('e','d',weight=2)
-G.add_edge('e','c',weight=1)
-G.add_edge('f','b',weight=0.4)
+G.add_edge(1,2,weight=1)
+G.add_edge(1,6,weight=0.5)
+G.add_edge(2,3,weight=0.5)
+G.add_edge(3,4,weight=0.5)
+G.add_edge(6,5,weight=0.6)
+G.add_edge(5,4,weight=2)
+G.add_edge(5,3,weight=1)
+G.add_edge(6,2,weight=0.4)
 
-nx.draw_networkx(G)
-plt.show()
-(length, path) = dijkstra(G, 'a' ,'d', weight='weight')
-print(length)
-print(path)
+#nx.draw_networkx(G)
+#plt.show()
+G1 = nx.to_dict_of_dicts(G)
+f = dijkstra(G1,1)
+print(f)
+#(length, path) = dijkstra(G, 'a' ,'d', weight='weight')
+#print(length)
+#print(path)
 
 '''
 (length1,path1) = shortest_path(G,'a','b',weight='weight')
